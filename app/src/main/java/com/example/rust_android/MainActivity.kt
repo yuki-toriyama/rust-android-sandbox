b033194a9814c8c8224cf4079d89320ed934026a
Yuuki Toriyama <yuki_toriyama@waku-2.com>
2022-10-17 16:21:18 +0900

    Rustの関数をKotlin側から呼ぶ処理を書いた

--- app/src/main/java/com/example/rust_android/MainActivity.kt
+++ app/src/main/java/com/example/rust_android/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.rust_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.sample_text).text = hello("山田さん")
+    }
+
+    external fun hello(name: String): String
+
+    companion object {
+        init {
+            System.loadLibrary("helloworld")
+        }
     }
 }
