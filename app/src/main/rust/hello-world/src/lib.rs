81841abf8fff1fc472ffd09720cb1e78c25cc738
Yuuki Toriyama <yuki_toriyama@waku-2.com>
2022-10-17 16:20:48 +0900

    Activity側から呼ばれるRustの関数を書いた

--- app/src/main/rust/hello-world/src/lib.rs
+++ app/src/main/rust/hello-world/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use jni::{
+    objects::{JClass, JString},
+    sys::jstring,
+    JNIEnv,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub unsafe extern "C" fn Java_com_example_rust_1android_MainActivity_hello(
+    env: JNIEnv,
+    _class: JClass,
+    input: JString,
+) -> jstring {
+    let input: String = env
+        .get_string(input)
+        .expect("Couldn't get java string!")
+        .into();
+    let output = env
+        .new_string(format!("Hello, {}!", input))
+        .expect("Couldn't create java string!");
+    output.into_inner()
 }
